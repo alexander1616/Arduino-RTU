e6790d7ab1cb31fac95f3089727537731f46841e
Alex Chan <alexander.c1989@gmail.com>
2023-02-21 14:46:24 -0500

    serial refine

--- ideprojmain/ideprojmain.ino
+++ ideprojmain/ideprojmain.ino
@@ -3,7 +3,7 @@
 #include "readLine.h"
 
 void setup(){
-	Serial.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop() {  
---
+++ ideprojmain/projectDef.h
@@ -0,0 +1,24 @@
+#ifndef projectDef_h
+#define projectDef_h 1
+
+#define t_OFF       0
+#define t_ON        1
+
+#define t_LED       2
+#define t_RED       5
+#define t_GREEN     6
+#define t_SET       10
+#define t_BLINK     11
+#define t_LEDS      12
+
+#define t_WORD      19 /* Requires 2 parts following */
+#define t_UNKNOWN   20
+#define t_STATUS    21
+#define t_HELP      22
+
+#define t_D13       30
+
+#define t_VERSION   254
+#define t_EOL       255
+
+#endif
