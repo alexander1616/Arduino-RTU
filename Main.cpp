079a3b60e0c9b776854039639d3bd61e6d903de1
Alex Chan <alexander.c1989@gmail.com>
2023-02-21 13:48:09 -0500

    testing commit

--- Main.cpp
+++ Main.cpp
@@ -1,12 +1,9 @@
 #include <Arduino.h>
 
 void setup(){
-	pinMode(13, OUTPUT);
+	Serial.begin(9600);
 }
 
 void loop(){
-	digitalWrite(13, HIGH); 
-	delay(1000);
-	digitalWrite(13, LOW);
-       	delay(1000);
+	Serial.println("Hello World");
 }
